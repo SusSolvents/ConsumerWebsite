d1ad48e4125c48593c99a2abfb1f984c049e8317
lennartboeckx <lennart_boeckx@hotmail.com>
2016-04-12 09:36:38 +0200

    setup

---
+++ DAL/EFAnalysis/AnalysisRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SS.DAL.EFAnalysis
+{
+    public class AnalysisRepository : IAnalysisRepository
+    {
+        private readonly EFDbContext context;
+
+        public AnalysisRepository()
+        {
+            this.context = new EFDbContext();
+        }
+    }
+}
---
+++ DAL/EFUser/UserRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SS.DAL.EFUser
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly EFDbContext context;
+
+        public UserRepository()
+        {
+            this.context = new EFDbContext();
+        }
+    }
+}
