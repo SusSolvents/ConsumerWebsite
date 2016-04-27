cfa37ab06c14a582ccb65db5343922478b2d0aca
lennartboeckx <lennart_boeckx@hotmail.com>
2016-04-27 15:16:53 +0200

    tabs on overview

--- UI-MVC/Scripts/Controllers/AnalysisOverviewController.js
+++ UI-MVC/Scripts/Controllers/AnalysisOverviewController.js
@@ -7,12 +7,22 @@
         }).success(function succesCallback(data) {
             console.log(data);
             for (var i = 0; i < data.AnalysisModels.length; i++) {
-                data.AnalysisModels[i].Model.AlgorithmName = Constants.AlgorithmName[data.AnalysisModels[0].Model.AlgorithmName];
+                data.AnalysisModels[i].Model.AlgorithmName = Constants.AlgorithmName[data.AnalysisModels[i].Model.AlgorithmName];
             }
             
             $scope.models = data.AnalysisModels;
             console.log(data.AnalysisModels);
         });
+
+        $scope.changetab = function changetab(event, name) {
+            $('ul.tabs-test li').removeClass('current');
+            $('.tab-content').removeClass('current');
+
+            $(event.currentTarget).addClass('current');
+            $("#" + name).addClass('current');
+        };
+
+        
     });
 
 app.constant('Constants', {
